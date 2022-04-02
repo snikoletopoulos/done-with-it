8e5cb1d4746cf005a378c908ae2a34cc25d8cf2e
Stavros <snikoletopoulos@hotmail.com>
2022-04-02 21:59:04 +0300

    Format code

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,18 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      [
-        "module-resolver",
-        {
-          root: ["."],
-          extentions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
-          alias: {
-            screens: "./screens",
-          },
-        },
-      ],
-    ],
-  };
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: [
+			[
+				"module-resolver",
+				{
+					root: ["."],
+					extentions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+					alias: {
+						screens: "./screens",
+					},
+				},
+			],
+		],
+	};
 };
