25a80d7ed8b68bab8aa46b5047dfb8bf8a4b1242
Stavros <snikoletopoulos@hotmail.com>
2022-05-01 22:41:04 +0300

    Finish Card component

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,4 +1,12 @@
-import { StyleSheet, View, Image } from "react-native";
+import {
+	StyleSheet,
+	View,
+	Image,
+	ImageSourcePropType,
+	ViewStyle,
+	ImageStyle,
+	TextStyle,
+} from "react-native";
 
 import colors from "constants/colors";
 
@@ -7,25 +15,56 @@ import AppText from "components/AppText";
 interface Props {
 	title: string;
 	subTitle: string;
-	image: string;
+	image: ImageSourcePropType;
 }
 
 const Card: React.FC<Props> = props => {
+	const { title, subTitle, image } = props;
+
 	return (
 		<View style={styles.card}>
-			<Image source={require(props.image)} />
-			<AppText>Card</AppText>
-			<AppText>Card</AppText>
+			<Image source={image} style={styles.image} />
+			<View style={styles.detailsContainer}>
+				<AppText style={styles.title}>{title}</AppText>
+				<AppText style={styles.subTitle}>{subTitle}</AppText>
+			</View>
 		</View>
 	);
 };
 
 export default Card;
 
-const styles = StyleSheet.create({
+interface Styles {
+	card: ViewStyle;
+	image: ImageStyle;
+	detailsContainer: ViewStyle;
+	title: TextStyle;
+	subTitle: TextStyle;
+}
+
+const styles = StyleSheet.create<Styles>({
 	card: {
 		borderRadius: 15,
 		backgroundColor: colors.white,
 		marginBottom: 20,
+		overflow: "hidden",
+	},
+
+	image: {
+		width: "100%",
+		height: 200,
+	},
+
+	detailsContainer: {
+		padding: 20,
+	},
+
+	title: {
+		marginBottom: 7,
+	},
+
+	subTitle: {
+		color: colors.secondary,
+		fontWeight: "bold",
 	},
 });
