34622abb355376acfb3837a179c773db4eae094a
Stavros <snikoletopoulos@hotmail.com>
2022-05-01 22:20:30 +0300

    Refine the WelcomeScreen component

---
+++ app/screens/WelcomeScreen.tsx
@@ -0,0 +1,78 @@
+import {
+	StyleSheet,
+	View,
+	ViewStyle,
+	Text,
+	Image,
+	ImageStyle,
+	ImageBackground,
+	TextStyle,
+} from "react-native";
+
+import colors from "../constants/colors";
+
+import AppButton from "components/AppButton";
+
+const WelcomeScreen: React.FC = () => {
+	return (
+		<ImageBackground
+			source={require("assets/images/background.jpg")}
+			style={styles.imageBackground}
+			blurRadius={10}
+		>
+			<View style={styles.logoContainer}>
+				<Image
+					style={styles.logo}
+					source={require("assets/images/logo-red.png")}
+				/>
+				<Text style={styles.tagLine}>Sell what you don't need</Text>
+			</View>
+
+			<View style={styles.buttonContainer}>
+				<AppButton color={"primary"} title="Login" onPress={() => {}} />
+				<AppButton color={"secondary"} title="Register" onPress={() => {}} />
+			</View>
+		</ImageBackground>
+	);
+};
+
+export default WelcomeScreen;
+
+interface Style {
+	imageBackground: ImageStyle;
+	logo: ImageStyle;
+	logoContainer: ViewStyle;
+	tagLine: TextStyle;
+	buttonContainer: ViewStyle;
+}
+
+const styles = StyleSheet.create<Style>({
+	imageBackground: {
+		flex: 1,
+		width: "100%",
+
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+
+	logo: {
+		width: 100,
+		height: 100,
+	},
+
+	logoContainer: {
+		marginTop: 30,
+		alignItems: "center",
+	},
+
+	tagLine: {
+		fontSize: 25,
+		fontWeight: "600",
+		paddingVertical: 20,
+	},
+
+	buttonContainer: {
+		width: "100%",
+		padding: 20,
+	},
+});
