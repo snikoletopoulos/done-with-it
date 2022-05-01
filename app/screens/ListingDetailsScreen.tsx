d37256f141ac009c0272360649eea3c8e7dda09d
Stavros <snikoletopoulos@hotmail.com>
2022-05-01 23:07:58 +0300

    Create ListingDetailScreen component

---
+++ app/screens/ListingDetailsScreen.tsx
@@ -0,0 +1,72 @@
+import {
+	StyleSheet,
+	View,
+	Image,
+	ImageStyle,
+	TextStyle,
+	ViewStyle,
+} from "react-native";
+
+import colors from "constants/colors";
+
+import AppText from "components/AppText";
+import ListItem from "components/ListItem";
+
+const ListingDetailsScreen: React.FC = () => {
+	return (
+		<View>
+			<Image
+				style={styles.image}
+				source={require("../assets/images/jacket.jpg")}
+			/>
+			<View style={styles.detailsContainer}>
+				<AppText style={styles.title}>Red jacket for sale</AppText>
+				<AppText style={styles.price}>$100</AppText>
+				<View style={styles.userContainer}>
+					<ListItem
+						image={require("../assets/images/mosh.jpg")}
+						title="Mosh Hamedani"
+						subTitle="5 Listings"
+					/>
+				</View>
+			</View>
+		</View>
+	);
+};
+
+export default ListingDetailsScreen;
+
+interface Styles {
+	image: ImageStyle;
+	detailsContainer: ViewStyle;
+	title: TextStyle;
+	price: TextStyle;
+	userContainer: ViewStyle;
+}
+
+const styles = StyleSheet.create<Styles>({
+	image: {
+		width: "100%",
+		height: 300,
+	},
+
+	detailsContainer: {
+		padding: 20,
+	},
+
+	title: {
+		fontSize: 24,
+		fontWeight: "500",
+	},
+
+	price: {
+		fontSize: 20,
+		fontWeight: "bold",
+		color: colors.secondary,
+		marginVertical: 10,
+	},
+
+	userContainer: {
+		marginVertical: 40,
+	},
+});
