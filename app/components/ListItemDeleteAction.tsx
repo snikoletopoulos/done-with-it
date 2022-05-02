e81b67bac0c61f68f65dac0f4b329761201f291f
Stavros <snikoletopoulos@hotmail.com>
2022-05-02 16:39:41 +0300

    Create ListItemDeketeAction component

---
+++ app/components/ListItemDeleteAction.tsx
@@ -0,0 +1,32 @@
+import { Pressable, StyleSheet, View, ViewStyle } from "react-native";
+
+import colors from "constants/colors";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+interface Props {
+	onPress?: React.ComponentProps<typeof Pressable>["onPress"];
+}
+
+const ListItemDeleteAction: React.FC<Props> = props => {
+	return (
+		<Pressable style={styles.container} onPress={props.onPress}>
+			<MaterialCommunityIcons name="trash-can" color="white" size={35} />
+		</Pressable>
+	);
+};
+
+export default ListItemDeleteAction;
+
+interface Styles {
+	container: ViewStyle;
+}
+
+const styles = StyleSheet.create<Styles>({
+	container: {
+		backgroundColor: colors.danger,
+		width: 70,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+});
