0c9f5ece1ea6cd73b7ab1473f99bd95f94a1b30c
Stavros <snikoletopoulos@hotmail.com>
2022-05-03 02:20:06 +0300

    Create Picker component

---
+++ app/components/ui/Picker/Picker.tsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import {
+	FlatList,
+	Modal,
+	Pressable,
+	StyleSheet,
+	TextStyle,
+	ViewStyle,
+} from "react-native";
+
+import colors from "constants/colors";
+import { Option, PickerItemProps } from "types/picker.types";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Text from "../Text";
+import Button from "../Button";
+import PickerItem from "./PickerItem";
+
+interface Props {
+	icon?: React.ComponentProps<typeof MaterialCommunityIcons>["name"];
+	placeholder?: string;
+	options?: Option[];
+	selectedItem?: Option;
+	onSelectItem: (item: Option) => void;
+	PickerItemComponent: React.FC<PickerItemProps>;
+}
+
+const AppPicker: React.FC<Props> = props => {
+	const {
+		icon,
+		placeholder,
+		options,
+		PickerItemComponent = PickerItem,
+	} = props;
+	const [modalIsVisible, setModalIsVisible] = useState(false);
+
+	return (
+		<>
+			<Pressable
+				style={styles.container}
+				onPress={() => setModalIsVisible(true)}
+			>
+				{icon && (
+					<MaterialCommunityIcons
+						name={icon}
+						size={20}
+						color={colors.medium}
+						style={styles.icon}
+					/>
+				)}
+				<Text style={styles.text}>
+					{props.selectedItem ? props.selectedItem.label : placeholder}
+				</Text>
+				<MaterialCommunityIcons
+					name="chevron-down"
+					size={20}
+					color={colors.medium}
+				/>
+			</Pressable>
+
+			<Modal visible={modalIsVisible} animationType="slide">
+				<Button title="Close" onPress={() => setModalIsVisible(false)} />
+				<FlatList
+					data={options}
+					keyExtractor={option => option.value.toString()}
+					renderItem={({ item }) => (
+						<PickerItemComponent
+							item={item}
+							onPress={() => {
+								props.onSelectItem(item);
+								setModalIsVisible(false);
+							}}
+						/>
+					)}
+				/>
+			</Modal>
+		</>
+	);
+};
+
+export default AppPicker;
+
+interface Styles {
+	container: ViewStyle;
+	icon: ViewStyle;
+	text: TextStyle;
+}
+
+const styles = StyleSheet.create<Styles>({
+	container: {
+		backgroundColor: colors.light,
+		borderRadius: 25,
+		flexDirection: "row",
+		width: "100%",
+		padding: 15,
+		marginVertical: 10,
+		alignItems: "center",
+	},
+
+	icon: {
+		marginRight: 10,
+	},
+
+	text: {
+		flex: 1,
+	},
+});
