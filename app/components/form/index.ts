bc370326cc6455b7de8492b010793ef2b54de384
Stavros <snikoletopoulos@hotmail.com>
2022-05-07 15:47:15 +0300

    Fix import paths

--- app/components/form/index.ts
+++ app/components/form/index.ts
@@ -1,5 +1,5 @@
-export { default as Form } from "./FormikForm";
 export { default as FormField } from "./FormField";
 export { default as ErrorMessage } from "./ErrorMessage";
 export { default as FormikForm } from "./FormikForm";
-export { default as SubitButton } from "./FormikForm";
+export { default as SubmitButton } from "./SubmitButton";
+export { default as FormPicker } from "./FormPicker";
