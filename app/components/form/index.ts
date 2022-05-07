851c9a3bf57b6b7e488a1f35c248f5b2bd82e205
Stavros <snikoletopoulos@hotmail.com>
2022-05-07 13:56:47 +0300

    Create file to export all the form components together

---
+++ app/components/form/index.ts
@@ -0,0 +1,5 @@
+export { default as Form } from "./FormikForm";
+export { default as FormField } from "./FormField";
+export { default as ErrorMessage } from "./ErrorMessage";
+export { default as FormikForm } from "./FormikForm";
+export { default as SubitButton } from "./FormikForm";
