296ca9804bcb5bb052611a22533794e02f034729
Stavros <snikoletopoulos@hotmail.com>
2022-05-07 15:48:29 +0300

    Create LoginScreen

---
+++ app/screens/LoginScreen.tsx
@@ -0,0 +1,83 @@
+import { StyleSheet, Image, ImageStyle, ViewStyle } from "react-native";
+
+import * as Yup from "yup";
+
+import Screen from "components/ui/Screen";
+import { FormField, FormikForm } from "components/form";
+import SubmitButton from "components/form/SubmitButton";
+
+export interface LoginFormValues {
+	email: string;
+	password: string;
+}
+
+const initialFormValues: LoginFormValues = {
+	email: "",
+	password: "",
+};
+
+const loginFormSchema = Yup.object().shape({
+	email: Yup.string().email().required().label("Email"),
+	password: Yup.string().required().min(6).label("Password"),
+});
+
+const LoginScreen: React.FC = () => {
+	return (
+		<Screen style={styles.container}>
+			<FormikForm
+				initialValues={initialFormValues}
+				validationSchema={loginFormSchema}
+				onSubmit={values => {
+					console.log(values);
+				}}
+			>
+				<>
+					<Image
+						style={styles.logo}
+						source={require("assets/images/logo-red.png")}
+					/>
+					<FormField
+						icon="email"
+						autoCapitalize="none"
+						autoCorrect={false}
+						keyboardType="email-address"
+						placeholder="Email"
+						textContentType="emailAddress"
+						name="email"
+					/>
+					<FormField
+						autoCapitalize="none"
+						autoCorrect={false}
+						icon="lock"
+						placeholder="Password"
+						secureTextEntry
+						textContentType="password"
+						name="password"
+					/>
+					<SubmitButton title="Login" />
+				</>
+			</FormikForm>
+		</Screen>
+	);
+};
+
+export default LoginScreen;
+
+interface Styles {
+	container: ViewStyle;
+	logo: ImageStyle;
+}
+
+const styles = StyleSheet.create<Styles>({
+	container: {
+		padding: 20,
+	},
+
+	logo: {
+		width: 80,
+		height: 80,
+		alignSelf: "center",
+		marginTop: 50,
+		marginBottom: 20,
+	},
+});
