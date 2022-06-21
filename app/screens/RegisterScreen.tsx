84e64f7f9ddaad00ba695ff26146738557260df8
Stavros <snikoletopoulos@hotmail.com>
2022-06-21 17:57:56 +0300

    Create Register page

---
+++ app/screens/RegisterScreen.tsx
@@ -0,0 +1,81 @@
+import { StyleSheet, ViewStyle } from "react-native";
+
+import * as Yup from "yup";
+import { AuthStackScreenProps } from "navigation/types";
+
+import Screen from "components/ui/Screen";
+import { FormField, FormikForm } from "components/form";
+import SubmitButton from "components/form/SubmitButton";
+
+export interface LoginFormValues {
+	name: string;
+	email: string;
+	password: string;
+}
+
+const initialFormValues: LoginFormValues = {
+	name: "",
+	email: "",
+	password: "",
+};
+
+const loginFormSchema = Yup.object().shape({
+	name: Yup.string().required().label("Name"),
+	email: Yup.string().email().required().label("Email"),
+	password: Yup.string().required().min(6).label("Password"),
+});
+
+const LoginScreen: React.FC<AuthStackScreenProps<"Login">> = () => {
+	return (
+		<Screen style={styles.container}>
+			<FormikForm
+				initialValues={initialFormValues}
+				validationSchema={loginFormSchema}
+				onSubmit={values => {
+					console.log(values);
+				}}
+			>
+				<>
+					<FormField
+						icon="person"
+						autoCapitalize="words"
+						autoCorrect={false}
+						placeholder="Name"
+						name="name"
+					/>
+					<FormField
+						icon="email"
+						autoCapitalize="none"
+						autoCorrect={false}
+						keyboardType="email-address"
+						placeholder="Email"
+						textContentType="emailAddress"
+						name="email"
+					/>
+					<FormField
+						autoCapitalize="none"
+						autoCorrect={false}
+						icon="lock"
+						placeholder="Password"
+						secureTextEntry
+						textContentType="password"
+						name="password"
+					/>
+					<SubmitButton title="Register" />
+				</>
+			</FormikForm>
+		</Screen>
+	);
+};
+
+export default LoginScreen;
+
+interface Styles {
+	container: ViewStyle;
+}
+
+const styles = StyleSheet.create<Styles>({
+	container: {
+		padding: 20,
+	},
+});
