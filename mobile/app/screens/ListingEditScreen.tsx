a6a6dffa3bdd37d66612c35f521e5c8d86bea120
Stavros <snikoletopoulos@hotmail.com>
2022-06-24 08:20:02 +0300

    Wrap form in a Screen component to keep everything visible

--- mobile/app/screens/ListingEditScreen.tsx
+++ mobile/app/screens/ListingEditScreen.tsx
@@ -1,6 +1,8 @@
 import * as Yup from "yup";
 import { categories } from "../../DUMMY_DATA";
 
+import useLocation from "hooks/use-location.hook";
+
 import {
 	FormikForm,
 	FormField,
@@ -8,7 +10,7 @@ import {
 	SubmitButton,
 } from "components/form";
 import FormImagePicker from "components/form/FormImagePicker";
-import useLocation from "hooks/use-location.hook";
+import Screen from "components/ui/Screen";
 
 const validationSchema = Yup.object().shape({
 	title: Yup.string().required().min(1).label("Title"),
@@ -22,44 +24,46 @@ const ListingEditScreen = () => {
 	const location = useLocation();
 
 	return (
-		<FormikForm
-			initialValues={{
-				title: "",
-				price: "",
-				decription: "",
-				category: null,
-				images: [],
-			}}
-			validationSchema={validationSchema}
-			onSubmit={values => {
-				console.log(values, location);
-			}}
-		>
-			<FormImagePicker name="images" />
-			<FormField maxLength={255} name="title" placeholder="Title" />
-			<FormField
-				keyboardType="numeric"
-				maxLength={8}
-				name="price"
-				placeholder="Price"
-				width={120}
-			/>
-			<FormPicker
-				item={categories}
-				name="category"
-				placeholder="Category"
-				width="50%"
-			/>
-			<FormField
-				maxLength={255}
-				multiline
-				name="description"
-				numberOfLines={3}
-				placeholder="Description"
-				width="100%"
-			/>
-			<SubmitButton title="Post" />
-		</FormikForm>
+		<Screen>
+			<FormikForm
+				initialValues={{
+					title: "",
+					price: "",
+					decription: "",
+					category: null,
+					images: [],
+				}}
+				validationSchema={validationSchema}
+				onSubmit={values => {
+					console.log(values, location);
+				}}
+			>
+				<FormImagePicker name="images" />
+				<FormField maxLength={255} name="title" placeholder="Title" />
+				<FormField
+					keyboardType="numeric"
+					maxLength={8}
+					name="price"
+					placeholder="Price"
+					width={120}
+				/>
+				<FormPicker
+					item={categories}
+					name="category"
+					placeholder="Category"
+					width="50%"
+				/>
+				<FormField
+					maxLength={255}
+					multiline
+					name="description"
+					numberOfLines={3}
+					placeholder="Description"
+					width="100%"
+				/>
+				<SubmitButton title="Post" />
+			</FormikForm>
+		</Screen>
 	);
 };
 
