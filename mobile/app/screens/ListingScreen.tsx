6fc21c89c0b5468262fdb5e21d210371e366ef71
Stavros <snikoletopoulos@hotmail.com>
2022-06-25 15:08:00 +0300

    Fix padding problem in SafeAreaView

--- mobile/app/screens/ListingScreen.tsx
+++ mobile/app/screens/ListingScreen.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, View, ViewStyle } from "react-native";
 
-import Constants from "expo-constants";
 import { FeedStackScreenProps } from "navigation/types";
 import listingsApi from "../api/listings";
 import { Listing } from "types/listing.types";
@@ -24,28 +23,37 @@ const ListingScreen: React.FC<FeedStackScreenProps<"Listings">> = props => {
 
 	return (
 		<Screen style={styles.screen}>
-			<FlatList
-				data={listings}
-				keyExtractor={listing => listing.id.toString()}
-				renderItem={({ item }) => (
-					<Card
-						title={item.title}
-						subTitle={`$${item.price}`}
-						imageUrl={item.images[0].url}
-						onPress={() => props.navigation.navigate("ListingDetails", item)}
-					/>
-				)}
-			/>
+			<View style={styles.container}>
+				<FlatList
+					data={listings}
+					keyExtractor={listing => listing.id.toString()}
+					renderItem={({ item }) => (
+						<Card
+							title={item.title}
+							subTitle={`$${item.price}`}
+							imageUrl={item.images[0].url}
+							onPress={() => props.navigation.navigate("ListingDetails", item)}
+						/>
+					)}
+				/>
+			</View>
 		</Screen>
 	);
 };
 
 export default ListingScreen;
 
-const styles = StyleSheet.create({
+interface Styles {
+	screen: ViewStyle;
+	container: ViewStyle;
+}
+
+const styles = StyleSheet.create<Styles>({
 	screen: {
-		padding: 20,
-		paddingTop: Constants.statusBarHeight + 20,
 		backgroundColor: colors.light,
+	},
+
+	container: {
+		padding: 20,
 	},
 });
