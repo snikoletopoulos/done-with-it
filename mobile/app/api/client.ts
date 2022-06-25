5b4235ef0a62218957e38b778acfd965f08d80b7
Stavros <snikoletopoulos@hotmail.com>
2022-06-26 02:10:17 +0300

    Update config files

--- mobile/app/api/client.ts
+++ mobile/app/api/client.ts
@@ -1,7 +1,7 @@
 import { create } from "apisauce";
 
 const apiClient = create({
-	baseURL: "http://192.168.1.14:9000/api",
+	baseURL: "http://192.168.135.240:9000/api",
 });
 
 export default apiClient;
