2b8aa96280d8725a4d6e0530854cdbb25811ce43
Stavros <snikoletopoulos@hotmail.com>
2022-06-26 21:09:41 +0300

    Cache all get requests and search the cache first if request fails

--- mobile/app/api/client.ts
+++ mobile/app/api/client.ts
@@ -1,7 +1,41 @@
-import { create } from "apisauce";
+import { ApiResponse, create } from "apisauce";
+import { AxiosRequestConfig } from "axios";
+
+import cache from "app/utilities/cache";
 
 const apiClient = create({
-	baseURL: "http://192.168.135.240:9000/api",
+	baseURL: "http://192.168.1.17:9000/api",
 });
+
+const axiosGet = apiClient.get;
+
+apiClient.get = async <T, U = T>(
+	url: string,
+	params?: object,
+	axiosConfig?: AxiosRequestConfig
+): Promise<ApiResponse<T, U>> => {
+	const response = await axiosGet<T, U>(url, params, axiosConfig);
+
+	if (response.ok) {
+		cache.store(url, response.data);
+		return response;
+	}
+
+	const cachedResponse = cache.get(url) as T;
+
+	return cachedResponse
+		? {
+				ok: true,
+				data: cachedResponse,
+				problem: null,
+				originalError: null,
+
+				status: response.status,
+				headers: response.headers,
+				config: axiosConfig,
+				duration: response.duration,
+		  }
+		: response;
+};
 
 export default apiClient;
