dff57ea805e804e5d3576136f718ae869eedb826
Stavros <snikoletopoulos@hotmail.com>
2022-06-27 22:22:24 +0300

    Remove unused imports

--- mobile/app/screens/ListingScreen.tsx
+++ mobile/app/screens/ListingScreen.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import { FlatList, StyleSheet, View, ViewStyle } from "react-native";
 
 import { FeedStackScreenProps } from "navigation/types";
