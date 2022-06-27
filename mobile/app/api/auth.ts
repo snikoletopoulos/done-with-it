03602445b6318cdbac8edf56ec7c5844105cc2b6
Stavros <snikoletopoulos@hotmail.com>
2022-06-27 21:51:52 +0300

    Type login service

--- mobile/app/api/auth.ts
+++ mobile/app/api/auth.ts
@@ -3,4 +3,4 @@ import client from "./client";
 const endpoint = "/auth";
 
 export const login = (email: string, password: string) =>
-	client.post(endpoint, { email, password });
+	client.post<string>(endpoint, { email, password });
