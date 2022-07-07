1142b761612a81826642f438bc46a9715298bb81
Stavros <snikoletopoulos@hotmail.com>
2022-07-07 10:12:36 +0300

    Add image file interface to express request object

--- server/src/custom.d.ts
+++ server/src/custom.d.ts
@@ -1,7 +1,18 @@
-
 declare module Express {
 	export interface Request {
 		user?: { userId: number; name: string; email: string };
 		images: any;
+		files?: ImageFile[];
 	}
+}
+
+interface ImageFile {
+	fieldname: string;
+	originalname: string;
+	encoding: string;
+	mimetype: string;
+	destination: string;
+	filename: string;
+	path: string;
+	size: number;
 }
