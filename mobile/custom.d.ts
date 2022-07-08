b4192b172b9f5cd4957857b311fe608e02f3a598
Stavros <snikoletopoulos@hotmail.com>
2022-07-08 09:59:09 +0300

    Exclude node_module from tsconfig and remove error

--- mobile/custom.d.ts
+++ mobile/custom.d.ts
@@ -2,6 +2,7 @@ import { RootTabParamList } from "./app/navigation/types";
 
 declare global {
 	namespace ReactNavigation {
+		//eslint-disable-next-line @typescript-eslint/no-empty-interface
 		interface RootParamList extends RootTabParamList {}
 	}
 }
